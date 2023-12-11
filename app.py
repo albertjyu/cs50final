4b13d0afee72be6ffca05291b746023871a3d4b3
Albert Yu <albert.jyu@gmail.com>
2023-12-10 21:22:58 -0800

    remove unused

--- app.py
+++ app.py
@@ -2,16 +2,13 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request
 from flask_session import Session
 
 # Configure App
 app = Flask(__name__)
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# Configure database
 db = SQL("sqlite:///leaderboard.db")
 
 @app.route("/")
