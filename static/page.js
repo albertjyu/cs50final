4bb718b350bc358d87a898d3472aa6ae16621142
Albert Yu <albert.jyu@gmail.com>
2023-12-10 18:26:57 -0800

    update1

--- static/page.js
+++ static/page.js
@@ -2,21 +2,66 @@
 const inputfield = document.getElementById("inputfield");
 const quotebox = document.getElementById("quotebox");
 
-async function prepareQuoteBox() {
-  // Get the word list using the fetch function
-  fetchRandomWords();
-  
-  // Fill the quotebox with the word list
-  let words = await populateQuote();
+async function preparePage() {
+  // Call the prepareQuoteBox function and assign its output to an array called words
+  var currentWord = 0;
+  var words = await populateQuoteBox(currentWord);
 
-  return words;
+  // Add an event listener to listen for when the first word is typed, then start the test and timer
+  inputfield.addEventListener("keydown", (key) => {
+    // Find which key is being pressed
+    let code = key.code;
+
+    // If the key is space (i.e. the user completes the current word being typed) then:
+    if (code === "Space") {
+      key.preventDefault(); // Prevent the space from being typed into the box
+
+      
+      // If the user typed the word correctly with no mispelling and case-sensitive
+      console.log(inputfield.value);
+      if (inputfield.value == words[currentWord]) {
+        quotebox.style.backgroundColor = "lightgreen";
+      } else quotebox.style.backgroundColor = "red";
+      
+      // reset the input text box to blank
+      inputfield.value = "";
+      currentWord++;
+      populateQuoteBox(currentWord);
+    }
+  });
+}
+preparePage();
+
+async function populateQuoteBox(currentWord) {
+  // Call the fetchRandomWords function and assign its output (list) to a constant named words
+  const words = await fetchRandomWords();
+  quotebox.innerHTML = ''
+
+  // Fill the word box with the random words
+  // for (word of words) {
+  //   quotebox.innerHTML = quotebox.innerHTML + " " + word;
+  // }
+  
+  index = 0;
+  for (word of words){
+    const span = document.createElement('span');
+    span.textContent = word + ' ';
+    if (index == currentWord){
+      span.style.backgroundColor = 'lightblue'
+    }
+    quotebox.appendChild(span);
+    index++;
+  }
+
+
+  return words
 }
 
 async function fetchRandomWords() {
   const NUMBEROFWORDS = 50;
   //const apiUrl = "https://random-word-api.herokuapp.com/word?number=30";
   const apiUrl =
-    "https://api.datamuse.com/words?ml=10+most+common+words+short+Words";
+    "https://api.datamuse.com/words?ml=duck&sp=b*&max=10";
 
   // Set variables for the async fetch request
   const response = await fetch(apiUrl);
@@ -32,42 +77,6 @@ async function fetchRandomWords() {
   return Object.keys(wordsObject);
 }
 
-async function populateQuote() {
-  // Call the fetchRandomWords function and assign its output (list) to a constant named words
-  const words = await fetchRandomWords();
 
-  // Fill the word box with the random words
-  for (word of words) {
-    quotebox.innerHTML = quotebox.innerHTML + " " + word;
-  }
-  
-  return words
-}
 
-async function preparePage() {
-  // Call the prepareQuoteBox function and assign its output to an array called words
-  var words = await prepareQuoteBox();
-  var currentWord = 0;
 
-  // Add an event listener to listen for when the first word is typed, then start the test and timer
-  inputfield.addEventListener("keydown", (key) => {
-    // Find which key is being pressed
-    let code = key.code;
-
-    // If the key is space (i.e. the user completes the current word being typed) then:
-    if (code === "Space") {
-      key.preventDefault(); // Prevent the space from being typed into the box
-
-      // If the user typed the word correctly with no mispelling and case-sensitive
-      console.log(inputfield.value);
-      if (inputfield.value == words[currentWord]) {
-        quotebox.style.backgroundColor = "green";
-      } else quotebox.style.backgroundColor = "red";
-
-      // reset the input text box to blank
-      inputfield.value = "";
-      currentWord++;
-    }
-  });
-}
-preparePage();
